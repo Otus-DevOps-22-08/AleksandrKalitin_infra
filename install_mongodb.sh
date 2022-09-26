062ba34a6bdd86fe177e067f67a21f74e7d629f3
AleksandrKalitin <aleksanderkalitin@yandex.ru>
2022-09-26 12:18:55 +0300

    4_hw/bugfix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+apt-get install -y sudo
+sudo apt-get install -y wget
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update
-apt-get install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
