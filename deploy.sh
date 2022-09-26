d1718c274cd1e20195897df77cc8944c31c1e943
AleksandrKalitin <aleksanderkalitin@yandex.ru>
2022-09-26 11:11:08 +0300

    cloud-testapp/4_hw/bugfix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt-get install git -y
+sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
 ps aux | grep puma
-
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo update
-sudo apt-get install -y ruby-full ruby-bundler build-essential
-
+sudo apt-get install -y ruby-full
+sudo apt-get install -y ruby-bundler
+sudo apt-get install -y build-essential
